0aee48bfce8dcf577183afe3a4d754b62b1fadc5
mjdierkes <mjdierkes@msn.com>
2022-01-15 18:13:41 -0600

    Initial Commit

---
+++ Gradual/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Gradual
+//
+//  Created by Mason Dierkes on 1/15/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Gradual/GradualApp.swift
@@ -0,0 +1,17 @@
+//
+//  GradualApp.swift
+//  Gradual
+//
+//  Created by Mason Dierkes on 1/15/22.
+//
+
+import SwiftUI
+
+@main
+struct GradualApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
